b0fd832f9e6bc4ae2ae8d053754eb56852a5471e
Eduard Herasym <edkido29@gmail.com>
2024-03-27 18:37:25 +0200

    make build

---
+++ index.b00f2db1.js
@@ -0,0 +1,2 @@
+var e=document.querySelector(".logo");e.addEventListener("click",function(){t.resolve()});var t=new Promise(function(t,n){e.addEventListener("click",function(){t()})}),n=new Promise(function(e,t){setTimeout(t,3e3)}),o=function(){var e=document.createElement("div");e.className="message",e.textContent="Promise was resolved!",document.body.append(e)},c=function(){var e=document.createElement("div");e.className="message error-message",e.textContent="Promise was rejected!",document.body.append(e)};t.then(o).catch(c),n.then(o).catch(c);
+//# sourceMappingURL=index.b00f2db1.js.map
